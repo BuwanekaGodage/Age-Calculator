de53f563d6fc56c7fe65b5433cfd8b93e51ace4a
BuwanekaGodage <82741598+BuwanekaGodage@users.noreply.github.com>
2021-04-29 13:48:29 +0530

    Add files via upload

---
+++ Age Calculator.py
@@ -0,0 +1,16 @@
+
+
+print("Please enter your Name and your age")
+name = input('Name:')
+print('What is your age?',name)
+age = int(input('age:'))
+days = age * 365
+minutes = age * 525948
+seconds = age * 1556926
+left_days = (55 * 365) - age
+years_left = (left_days/365)
+print(name,'has been alive for',days,'days',minutes,'minutes',seconds,'seconds')
+print("How many days do you have left to live (average age is 70)")
+print(left_days)
+print('thank you',name,'for using my little age calculation software..')
+print ('Creator, Buwaneka Widhurinda Godage')
